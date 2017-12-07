1d0bed50d1a8c5ba9ebaf2edf2bd6c84280e97f2
danielle bowers <daniellebowers@danielles-MacBook-Pro.local>
2017-12-06 15:31:38 -1000

    Checkpoint 4 completed

---
+++ app/scripts/Message.js
@@ -0,0 +1,17 @@
+(function() {
+  function Message($firebaseArray) {
+    var Message = {};
+    var ref = firebase.database().ref().child("messages");
+    var messages = $firebaseArray(ref);
+
+    Message.getByRoomId = function(roomID) {
+        return $firebaseArray(ref.orderByChild('roomID').equalTo(roomID));
+    };
+
+    return Message;
+    };
+
+  angular
+    .module('blocChat')
+    .factory('Message', ['$firebaseArray', Message]);
+})();
--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,10 +1,17 @@
 (function() {
-    function HomeCtrl(Room) {
+    function HomeCtrl(Room, Message) {
       this.Rooms = Room.all;
+      this.messages=null;
+
+
+      this.selectRoom=function(room){
+        this.currentRoom=room;
+        this.messages=Message.getByRoomId(room.$id);
+      }
 
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', HomeCtrl]);
 })();
