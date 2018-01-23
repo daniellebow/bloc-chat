7bd7576aee449af7f8f924639e7025aff89fe925
danielle bowers <daniellebowers@danielles-MacBook-Pro.local>
2018-01-23 08:47:05 -1000

    checkpoint send messages completed

--- app/scripts/Message.js
+++ app/scripts/Message.js
@@ -4,9 +4,15 @@
     var ref = firebase.database().ref().child("messages");
     var messages = $firebaseArray(ref);
 
+    Message.all=messages;
+
     Message.getByRoomId = function(roomID) {
         return $firebaseArray(ref.orderByChild('roomID').equalTo(roomID));
     };
+
+    Message.send = function(message) {
+       messages.$add(message);
+   };
 
     return Message;
     };
--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -2,13 +2,19 @@
     function HomeCtrl(Room, Message, $cookies) {
       this.Rooms = Room.all;
       this.messages=null;
-      this.userName = $cookies.get('blocChatCurrentUser');
+      this.currentRoom=null;
+      this.username = $cookies.get('blocChatCurrentUser');
 
       this.selectRoom=function(room){
         this.currentRoom=room;
         this.messages=Message.getByRoomId(room.$id);
       }
-
+      this.sendMessage = function () {
+          this.message.roomID = this.currentRoom.$id;
+          this.message.username = this.username;
+          this.message.sentAt = firebase.database.ServerValue.TIMESTAMP;
+          Message.send(this.message);
+      }
     }
 
     angular
